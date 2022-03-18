825baa90fcfb45453f6a43468b16a91bed8c6061
anuragrayabharapu <96014584+anuragrayabharapu@users.noreply.github.com>
2022-03-18 17:47:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const fs=require('fs');
+const PORT = process.env.PORT||5000;
+const path= require('path');
+
+const INDEX="/index.html";
+const app= express();
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,'public',INDEX));
+});
+
+app.listen(PORT,()=>{console.log('listening');});
